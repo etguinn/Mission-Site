f0bd12ac8d887abe07f72cd5f356950331689c55
Ethan Guinn <et.guinn@gmail.com>
2018-11-10 11:51:58 -0700

    - Set up mission methods - Created dynamic settings for MissionFAQ view - Added breadcrumbs - Added photos - Added reply box under each question in the MissionFAQ view

--- MissionSite_Project1_Team1-3/Controllers/HomeController.cs
+++ MissionSite_Project1_Team1-3/Controllers/HomeController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Team 1-3: Tiara Johnson, Bailey Coleman, HaiJung Hsu, and Ethan Guinn
+    Description: This website offers specific information about missions 
+                as well as opportunities for user to both ask new questions 
+                and give answers regarding specific mission details.
+    Date: 11/10/2018
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,6 +37,44 @@ namespace MissionSite_Project1_Team1_3.Controllers
         public ActionResult Missions()
         {
             return View();
+        }
+
+        public ActionResult MissionFAQ(string MissionName)
+        {
+            return View();
+        }
+
+        public ActionResult LondonEngland()
+        {
+            ViewBag.mTitle = "London England";
+            ViewBag.mFlag = "";
+            ViewBag.mPresident = "";
+            ViewBag.mAddress = "";
+            ViewBag.mLanguage = "";
+            ViewBag.mReligion = "";
+            return View("MissionFAQ");
+        }
+
+        public ActionResult ChinaHongKong()
+        {
+            ViewBag.mTitle = "China Hong Kong";
+            ViewBag.mFlag = "/Content/Images/HongKongFlag.png";
+            ViewBag.mPresident = "President Maurice Lam";
+            ViewBag.mAddress = "18 Dorset Crescent, Kowloon Tong, Kowloon, Hong Kong";
+            ViewBag.mLanguage = "Cantonese, Mandarin, English, and Tagalog";
+            ViewBag.mReligion = "Ancestor worship, Buddhism, Taoism, and Christianity";
+            return View("MissionFAQ");
+        }
+
+        public ActionResult UtahOgden()
+        {
+            ViewBag.mTitle = "Utah Ogden";
+            ViewBag.mFlag = "";
+            ViewBag.mPresident = "";
+            ViewBag.mAddress = "";
+            ViewBag.mLanguage = "";
+            ViewBag.mReligion = "";
+            return View("MissionFAQ");
         }
     }
 }
